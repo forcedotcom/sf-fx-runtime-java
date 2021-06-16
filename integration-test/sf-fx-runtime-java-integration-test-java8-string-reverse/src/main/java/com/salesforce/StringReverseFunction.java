04047d2fe7e7d179b4a3f8c3c80584f8f82542e3
Manuel Fuchs <manuel.fuchs@salesforce.com>
2021-06-16 10:54:29 +0200

    Add first integration test

---
+++ integration-test/sf-fx-runtime-java-integration-test-java8-string-reverse/src/main/java/com/salesforce/StringReverseFunction.java
@@ -0,0 +1,12 @@
+package com.salesforce;
+
+import com.salesforce.functions.jvm.sdk.Context;
+import com.salesforce.functions.jvm.sdk.InvocationEvent;
+import com.salesforce.functions.jvm.sdk.SalesforceFunction;
+
+public class StringReverseFunction implements SalesforceFunction<String, String> {
+    @Override
+    public String apply(InvocationEvent<String> event, Context context) throws Exception {
+        return new StringBuilder(event.getData()).reverse().toString();
+    }
+}
