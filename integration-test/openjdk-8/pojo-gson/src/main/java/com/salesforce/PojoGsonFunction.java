19e6d14edad49454a46d16a2d3a2aca6ed3a8f9b
Manuel Fuchs <malax@malax.de>
2021-06-16 13:33:07 +0200

    Integration Tests (#108)
    * Add first integration test
    
    * Add more integration tests
    
    * Add integration tests for Java 11

---
+++ integration-test/openjdk-8/pojo-gson/src/main/java/com/salesforce/PojoGsonFunction.java
@@ -0,0 +1,16 @@
+package com.salesforce;
+
+import com.salesforce.functions.jvm.sdk.Context;
+import com.salesforce.functions.jvm.sdk.InvocationEvent;
+import com.salesforce.functions.jvm.sdk.SalesforceFunction;
+
+public class PojoGsonFunction implements SalesforceFunction<Input, Output> {
+    @Override
+    public Output apply(InvocationEvent<Input> event, Context context) throws Exception {
+        Input data = event.getData();
+
+        return new Output(
+            String.format("Hello %s, you are %d years old!", data.getName(), data.getAge())
+        );
+    }
+}
