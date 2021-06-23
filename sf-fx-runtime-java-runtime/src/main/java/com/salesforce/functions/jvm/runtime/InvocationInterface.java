c7c9d0461eb44accb53bcdfea35e6cd8f329abef
Manuel Fuchs <malax@malax.de>
2021-06-23 13:12:05 +0200

    Add unit tests for UndertowInvocationInterface (#113)
    * Add unit tests for UndertowInvocationInterface
    
    * Ensure serve does not terminate immediately
    
    * Remove content type check for CloudEvent data
    
    This check was introduced late in the development cycle but breaks existing implementations.
    
    * Fix UndertowInvocationInterface#isStarted()

--- sf-fx-runtime-java-runtime/src/main/java/com/salesforce/functions/jvm/runtime/InvocationInterface.java
+++ sf-fx-runtime-java-runtime/src/main/java/com/salesforce/functions/jvm/runtime/InvocationInterface.java
@@ -15,11 +15,35 @@ import com.salesforce.functions.jvm.runtime.project.ProjectFunction;
  */
 public interface InvocationInterface<T, R, E extends Throwable> {
   /**
-   * Starts the InvocationInterface for the given function. Implementations are supposed to block
-   * until they terminate.
+   * Starts the InvocationInterface for the given function. Implementations are supposed to return
+   * as soon as they are able to serve function invocations.
    *
    * @param projectFunction The function to expose via this InvocationInterface.
    * @throws Exception When the implementation cannot recover from an error and needs to terminate.
+   * @see #stop()
    */
   void start(ProjectFunction<T, R, E> projectFunction) throws Exception;
+
+  /**
+   * Stops the InvocationInterface.
+   *
+   * @throws Exception When the implementation cannot recover from an error and needs to terminate.
+   * @see #start(ProjectFunction)
+   */
+  void stop() throws Exception;
+
+  /**
+   * Blocks until the InvocationInterface wants to stop itself.
+   *
+   * @throws Exception When the implementation cannot recover from an error and needs to terminate.
+   */
+  void block() throws Exception;
+
+  /**
+   * Returns if the InvocationInterface is started.
+   *
+   * @return If the InvocationInterface is started.
+   * @see #start(ProjectFunction)
+   */
+  boolean isStarted();
 }
