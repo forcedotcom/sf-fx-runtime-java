e8828613969b5bd0f9de88104b19cca30ed2f030
Manuel Fuchs <manuel.fuchs@salesforce.com>
2021-07-29 10:37:22 +0200

    Fix quoting and escaping in DefaultLoggingFormatterTest

--- sf-fx-runtime-java-logger/src/test/java/com/salesforce/functions/jvm/runtime/logger/DefaultLoggingFormatterTest.java
+++ sf-fx-runtime-java-logger/src/test/java/com/salesforce/functions/jvm/runtime/logger/DefaultLoggingFormatterTest.java
@@ -34,7 +34,7 @@ public class DefaultLoggingFormatterTest {
         result,
         is(
             equalTo(
-                "\"localDateTime\"=\"1970-01-01T00:00:00Z\" \"level\"=\"DEBUG\" \"loggerName\"=\"foo.bar.baz\" \"message\"=\"This is a message!\" \"invocationId\"=\"e3a4ae2b-fefb-4277-89d0-7068e7e39b99\" \n")));
+                "localDateTime=1970-01-01T00:00:00Z level=DEBUG loggerName=foo.bar.baz message=\"This is a message!\" invocationId=e3a4ae2b-fefb-4277-89d0-7068e7e39b99")));
   }
 
   @Test
@@ -49,7 +49,7 @@ public class DefaultLoggingFormatterTest {
         result,
         is(
             equalTo(
-                "\"localDateTime\"=\"1970-01-01T00:00:00Z\" \"level\"=\"WARN\" \"loggerName\"=\"c.s.f.jvm.runtime.logger.ClassName\" \"message\"=\"This is a message!\" \"invocationId\"=\"e3a4ae2b-fefb-4277-89d0-7068e7e39b99\" \n")));
+                "localDateTime=1970-01-01T00:00:00Z level=WARN loggerName=c.s.f.jvm.runtime.logger.ClassName message=\"This is a message!\" invocationId=e3a4ae2b-fefb-4277-89d0-7068e7e39b99")));
   }
 
   @Test
@@ -65,7 +65,7 @@ public class DefaultLoggingFormatterTest {
         result,
         is(
             equalTo(
-                "\"localDateTime\"=\"1970-01-01T00:00:00Z\" \"level\"=\"TRACE\" \"loggerName\"=\"c.s.f.jvm.runtime.logger.EmptyMDC\" \"message\"=\"This is a message!\" \"invocationId\"=\"null\" \n")));
+                "localDateTime=1970-01-01T00:00:00Z level=TRACE loggerName=c.s.f.jvm.runtime.logger.EmptyMDC message=\"This is a message!\" invocationId=null")));
   }
 
   @Test
@@ -76,6 +76,6 @@ public class DefaultLoggingFormatterTest {
         result,
         is(
             equalTo(
-                "\"localDateTime\"=\"1970-01-01T00:00:00Z\" \"level\"=\"INFO\" \"loggerName\"=\"blank\" \"message\"=\"Checking backslash \"test\"\" \"invocationId\"=\"e3a4ae2b-fefb-4277-89d0-7068e7e39b99\" \n")));
+                "localDateTime=1970-01-01T00:00:00Z level=INFO loggerName=blank message=\"Checking backslash \\\"test\\\"\" invocationId=e3a4ae2b-fefb-4277-89d0-7068e7e39b99")));
   }
 }
